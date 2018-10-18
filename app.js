2a33b8cb774154ea472d5e157e0da0fa2d1b0fc0
Pamela Hutchinson <pamelahutchinson@Pamelas-MBP.localdomain>
2018-10-18 15:45:50 -0500

    added the function to view shoppping bag

--- app.js
+++ app.js
@@ -1,38 +1,75 @@
 //Getting access to the different inputs
 let groceryLists = document.getElementById('groceryLists');
-let btnAddNew = document.getElementById('addNew');
+let btnAddNew = document.getElementById('addNewShop');
+let seeAllOrders = document.getElementById('seeAllOrders')
 let newShopTextBox = document.getElementById('newShopBox');
-let newItemTextBox = document.getElementById('newItemBox');
-let list = document.createElement('li')
+let groceryList = document.getElementById('groceryList');
+
 
 //Creating an instance of firebase realtime database
 const database = firebase.database();
 //Create a node under the rootNode and call its Items
-const shoppingRef = database.ref();
+const marketsRef = database.ref("markets");
 
-let groceries = [] //An empty array to hold all the grocery lists
+let markets = [] //An empty array to hold all the grocery lists
 
 btnAddNew.addEventListener('click',function(){
-
-    let newShop = newShopTextBox.value 
-    let newItem = newItemTextBox.value
-    // list.appendChild(newItem)
-
-    //create a grocery list value
-    let groceryList = {Shop: newShop, Item : newItem};
-
-    addNewGroceries(groceryList);
+    let newShopTitle = newShopTextBox.value 
+    let newShop = {title : newShopTitle}
+    
+    //create an object to hold the title
+saveMarket(newShop)
 
 });
 
-function addNewGroceries(groceryList){
+
+function saveMarket(newShop){
+    console.log("I am working")
+    marketsRef.child(newShop.title).set(newShop)
+};
+
+
+function addNewGroceries(btn, new_shop) {
+
+    let groceryItemTitle = btn.previousElementSibling.value
     
-    let shopRef = shoppingRef.child(groceryList.Shop)
-    shopRef.set({
-        name : groceryList.Shop, 
-        items : [groceryList.Item]
+    let itemsRef = marketsRef.child(new_shop).child("items")
+    itemsRef.child(groceryItemTitle).set({
+        title: groceryItemTitle})
+    };
+
+function displayMarkets(){
+    console.log("Hey cutie patuti")
+    let liItems = markets.map(function(newShop){
+        return `<li>
+        <label>newShopTitle</label>
+        <input type="text" placeholder = "Enter grocery item" />
+        <button onclick="addNewGroceries(this),'${newShop.title}')">Save Grocery Item</button>
+        </li>`
     })
 
-    //database.ref(groceryList.Shop).set(groceryList.Item)
-
+   groceryList.innerHTML = liItems.join('');
 };
+
+
+function configureObservers(){
+    console.log("Hellu cutie")
+    marketsRef.on('value', function(snapshot){
+        markets = []
+        snapshot.forEach(function(childSnapshot){
+            // console.log(childSnapshot.key)
+            console.log(childSnapshot.val())
+            console.log(groceryList)
+        markets.push(childSnapshot.val())
+        })
+
+        //display categories on the screen
+        displayMarkets()
+    })
+}
+
+seeAllOrders.addEventListener('click',function() {
+    configureObservers()
+})
+
+
